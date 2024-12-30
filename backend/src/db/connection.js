3215f25424b2757119d306f22a4cb124ee66c7ba
eralonso <eric.alonso2003@gmail.com>
2024-12-30 14:17:31 +0100

    Made db collections models

--- backend/src/db/connection.js
+++ backend/src/db/connection.js
@@ -1,6 +1,10 @@
 import { MongoClient } from "mongodb";
 
-// import BoardSchema from "./models/Board.json" with { type: json };
+import UserSchema from "../models/User.json" with { type: "json" };
+import BoardSchema from "../models/Board.json" with { type: "json" };
+import ListSchema from "../models/List.json" with { type: "json" };
+import TaskSchema from "../models/Task.json" with { type: "json" };
+import LabelSchema from "../models/Label.json" with { type: "json" };
 
 const URI = process.env.DB_URI || "";
 const client = new MongoClient(URI);
@@ -9,16 +13,40 @@ let	db;
 
 try {
   await client.connect();
-  db = await client.db("flowboard");
+  db = await client.db("database");
 
 	db.command({ ping: 1 });
   console.log("You successfully connected to MongoDB!");
 
-	// db.createCollection("boards", {
-	// 	validator: {
-	// 		$jsonSchema: BoardSchema
-	// 	}
-	// });
+	db.createCollection("users", {
+		validator: {
+			$jsonSchema: UserSchema
+		}
+	});
+
+	db.createCollection("boards", {
+		validator: {
+			$jsonSchema: BoardSchema
+		}
+	});
+
+	db.createCollection("lists", {
+		validator: {
+			$jsonSchema: ListSchema
+		}
+	});
+
+	db.createCollection("tasks", {
+		validator: {
+			$jsonSchema: TaskSchema
+		}
+	});
+
+	db.createCollection("labels", {
+		validator: {
+			$jsonSchema: LabelSchema
+		}
+	});
 } catch (err) {
   console.error(err);
 }

840eb42c3c35345e899192c84352ed31fb97bdde
eralonso <eric.alonso2003@gmail.com>
2025-01-04 14:10:12 +0100

    Making query to backend to create new board

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -111,29 +111,45 @@ function	Carousel({direction, innerContainer, cards, CardComponent}) {
 function	NewBoard() {
 	const	[isPopup, setIsPopup] = useState(false);
 
-	const	popupCreateNewBoard = () => {
-		// alert("create new board");
-		setIsPopup(true);
+	const	handleSubmit = (e) => {
+		e.preventDefault();
+
+		const data = Object.fromEntries(new FormData(e.target));
+
+		console.log(data);
+		fetch("/api/board/new", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(data),
+		})
+			.then(data => data.json())
+			.then(data => console.log(data))
+			.catch(err => console.log(err))
+		setIsPopup(false);
+		return ;
 	}
 
 	return (
 		<>
-			<button className="bg-transparent" onClick={popupCreateNewBoard}>
+			<button className="bg-transparent" onClick={() => setIsPopup(true)}>
 				<h3 className="font-bold text-btn-blue text-[0.625rem]">New</h3>
 			</button>
 			{
 				isPopup && 
 					<div className="absolute top-0 left-0 w-screen h-screen bg-blurBackground-dark/75 flex justify-center items-center">
-						<div className="w-[19.75rem] h-[9.688rem] bg-white rounded-[0.625rem] p-2 drop-shadow">
+						<div className="w-[19.75rem] h-[9.688rem] bg-white rounded-[0.625rem] p-4 drop-shadow flex flex-col justify-around">
 							<div className="flex justify-between">
-								<h2 className="text-sm">Create Board</h2>
+								<h2 className="flex-1 text-sm text-center">Create Board</h2>
 								<button onClick={() => setIsPopup(false)}>
 									<img src="/icons/close.svg"/>
 								</button>
 							</div>
-							<form action="/api/boards/new" className="flex flex-col justify-center items-center gap-2">
-								<label for="name" className="text-[0.625rem]">Title of the board *</label><br/>
-								<input type="text" id="name" name="name" placeholder="Homework, etc" className="w-[90%] py-2 px-2 bg-btn-grey-selected rounded-[0.313rem] shadow-inner"/><br/>
+							<form onSubmit={handleSubmit} className="flex flex-col justify-center items-center gap-y-2">
+								<label htmlFor="name" className="w-full text-[0.625rem]">Title of the board *
+									<input type="text" id="name" name="name" placeholder="Homework, etc" className="w-full py-2 px-2 bg-btn-grey-selected rounded-[0.313rem] shadow-inner text-sm"/><br/>
+								</label>
 								<input type="submit" value="Create" className="bg-btn-grey-optionSelected rounded-[0.625rem] text-sm py-1 px-4 drop-shadow-md"/>
 							</form>
 						</div>
@@ -145,7 +161,7 @@ function	NewBoard() {
 
 function	BoardsListLayout({title, boards, create, direction}) {
 	return (
-		<div className="flex flex-col w-[95%]">
+		<div className="flex flex-col w-[95%] overflow-hidden">
 			<div className="flex justify-between">
 				<h1 className="font-semibold text-xl">{title}</h1>
 				{ create && <NewBoard/> }

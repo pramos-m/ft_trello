4eb7b80971b2c096fa5ef824298e09d126c58f6b
eralonso <eric.alonso2003@gmail.com>
2025-01-04 19:39:14 +0100

    Removed non-utilities from Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,113 +1,13 @@
-import { useState } from "react";
-import Logout from "../components/Logout.jsx";
+import { Logout } from "../components/Logout.jsx";
 import { BoardsListLayout } from "../components/BoardsListLayout.jsx";
 
-// function	Carousel({cards}) {
-// 	const	[dragInfo, setDragInfo] = useState({isPressed: false, left: 0});
-
-// 	const checkBoundary = e => {
-//     let outer = e.target.getBoundingClientRect();
-//     let inner = e.target.childNodes[1].getBoundingClientRect();
-
-//     if (parseInt(e.target.childNodes[1].style.left) > 0) {
-// 			setDragInfo(prev => ({
-// 				...prev,
-// 				left: 0
-// 			}));
-// 			return ;
-//     }
-//     if (inner.right < outer.right) {
-// 			setDragInfo(prev => ({
-// 				...prev,
-// 				left: -(inner.width - outer.width),
-// 			}));
-//     }
-
-// 		return ;
-// 	};
-
-// 	const	carouselDragStart = (e) => {
-// 		setDragInfo(prev => ({
-// 			...prev,
-// 			isPressed: true,
-// 			cursorX: e.clientX - e.target.childNodes[1].offsetLeft
-// 		}));
-// 		// console.log(e);
-// 		// console.log("client x: ", e.clientX);
-// 		// console.log(e.target.childNodes[1]);
-// 		console.log("offsetLeft: ", e.target.childNodes[1].offsetLeft);
-//     e.target.style.cursor = "grabbing";
-// 		return ;
-// 	}
-
-// 	const	carouselDragMove = (e) => {
-// 		if (!dragInfo.isPressed)
-// 			return ;
-// 		e.preventDefault();
-// 		// console.log(e);
-// 		// e.preventDefault().
-//     setDragInfo(prev => ({
-// 			...prev,
-// 			left: e.clientX - prev.left
-// 		}));
-// 		checkBoundary(e);
-// 		return ;
-// 	}
-
-// 	const	carouselDragEnd = (e) => {
-// 		setDragInfo(prev => ({
-// 			...prev,
-// 			isPressed: false,
-// 		}));
-// 		e.target.style.cursor = "grab";
-// 		return ;
-// 	}
-
-// 	console.log(dragInfo);
-// 	return (
-// 		<div className="w-[95%] h-1/5 overflow-hidden absolute top-[15%]"
-// 			onMouseDown={carouselDragStart} onMouseMove={carouselDragMove} onMouseUp={carouselDragEnd}>
-// 			<h1 className="mb-4 pointer-events-none">Favorite</h1>
-// 			<div className={`w-full h-[70%] flex gap-4 pointer-events-none absolute top-[35%]`}
-// 				style={{left: `${dragInfo.left}px`}} >
-// 				{
-// 					[...Array(8).keys()].map(i =>
-// 						<div key={`Patata ${i}`} className="flex-none w-[60%] h-full border-2 rounded-xl flex justify-center items-center">
-// 							Patata {i}
-// 						</div>
-// 					)
-// 				}
-// 			</div>
-// 		</div>
-// 	);
-// }
-
-function	BoardsList() {
-	return (
-		<div>
-		</div>
-	);
-}
-
-function	Carousel({direction, innerContainer, cards, CardComponent}) {
-	return (
-		<div>
-			{
-				cards && cards.map(card => 
-					<CardComponent key={card.id} {...card} />
-				)
-			}
-		</div>
-	);
-}
-
 function	Home() {
-	let	boards = []
+	let	boards = [];
 	let	categories = [
 		{name: "Favorite", filter: ({favorite}) => favorite, create: false, direction: "x"},
 		{name: "Recent", filter: ({recent}) => recent, create: false, direction: "x"},
 		{name: "All", filter: () => true, create: true, direction: "y"},
-	]
+	];
 
 	for (let i = 0; i < 10; i++)
 		boards.push({
@@ -140,7 +40,4 @@ function	Home() {
 // <Carousel direction="x" innerContainer={BoardsListLayout} cards={boards.filter(({recent}) => recent)} CardComponent={BoardPreview}/>
 // <Carousel direction="y" innerContainer={BoardsListLayout} cards={boards} CardComponent={BoardPreview}/>
 
-//<RowCarousel title="Favorite" boards={boards}/>
-//<RowCarousel title="Recent" boards={boards}/>
-//<ColumnCarousel title="All" boards={boards}/>
 export default Home;

8543af023d20f6f5f575959d4bc6ffe6fd2b26ae
eralonso <eralonso@cbr15s6.42barcelona.com>
2025-01-14 12:45:05 +0100

    Creating an alternative to hook for boards

---
+++ frontend/src/hooks/MyUseBoard.js
@@ -0,0 +1,29 @@
+import { useParams } from "react-router";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router";
+
+import { getBoard, deleteBoard } from "../services/boards";
+
+function	useBoard() {
+	const	{ boardId: id } = useParams();
+	const	[board, setBoard] = useState({});
+	const	[error, setError] = useState(null);
+	const	navigate = useNavigate();
+
+	const	refreshBoard = () => {
+		getBoard({id})
+			.then(board => setBoard(board))
+			.catch(err => setError(err));
+	};
+
+	const	removeBoard = () => {
+		deleteBoard({id})
+			.then(({succes}) => succes ? navigate("/") : console.error(";-;"))
+			.catch(err => setError(err))
+	};
+
+	useEffect(refreshBoard, id);
+	return ([board, refreshBoard, removeBoard, error]);
+}
+
+export default useBoard;
